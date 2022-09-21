ba6b5c10e43c23dc755f610e587a2ef23a7b7085
Lucas Silva <lucash.silva1612@gmail.com>
2022-09-20 21:56:29 -0300

    remove api module

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
 
-  scope :api, constraints: AcceptJsonConstraint.new do
-    namespace :v1 do
-      resources :health, only: :index
-    end
+  scope :api, defaults: { format: :json } do
+    resources :health, only: :index
   end
 end
--- spec/requests/health_spec.rb
+++ spec/requests/health_spec.rb
@@ -2,10 +2,10 @@
 
 require 'rails_helper'
 
-RSpec.describe 'V1::Health', type: :request do
+RSpec.describe 'Health', type: :request do
   describe 'GET /index' do
     it 'returns http success' do
-      get '/api/v1/health', headers: { 'Accept' => 'application/json' }
+      get '/api/health'
 
       expect(response).to have_http_status(:ok)
     end
